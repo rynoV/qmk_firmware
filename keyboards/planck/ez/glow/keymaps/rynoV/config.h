d10ef1a547d2c4c410f83caa819743a7cafc3af8
Calum Sieppert <sieppertcalum@gmail.com>
2022-06-04 18:47:44 -0600

    Config version working with zsa/qmk_firware firmware20
    firmware20 commit 89ea06a18b21683243d284f8ca69c5b4ed6ba5e7
    
    This is just a backup after I switched to the latest qmk version

---
+++ keyboards/planck/ez/glow/keymaps/rynoV/config.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#ifdef AUDIO_ENABLE
+#define STARTUP_SONG SONG(PLANCK_SOUND)
+#endif
+
+#define MIDI_BASIC
+
+#define ENCODER_RESOLUTION 4
+
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+#define ORYX_CONFIGURATOR
+#undef IGNORE_MOD_TAP_INTERRUPT
+
+#undef RGB_DISABLE_TIMEOUT
+#define RGB_DISABLE_TIMEOUT 900000
+
+#define USB_SUSPEND_WAKEUP_DELAY 0
+#undef MOUSEKEY_WHEEL_DELAY
+#define MOUSEKEY_WHEEL_DELAY 200
+
+#define HOLD_ON_OTHER_KEY_PRESS
+#define FIRMWARE_VERSION u8"BzKJQ/ADDyo"
+#define RGB_MATRIX_STARTUP_SPD 60
